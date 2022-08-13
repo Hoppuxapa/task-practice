6a2fce2ddffb7610fee82d801758ae7dae93ba60
Ruslan Kravchuk <rkravchuk@infomedia.com.au>
2022-08-13 21:07:21 +0300

    - add Hangman game - add the longest common prefix

---
+++ task-practice/src/main/java/longest/common/prefix/LongestCommonPrefix.java
@@ -0,0 +1,27 @@
+package longest.common.prefix;
+
+import java.util.Scanner;
+
+public class LongestCommonPrefix {
+
+    Scanner scanner = new Scanner(System.in);
+    private String s1;
+    private String s2;
+
+    public String getCommonPrefix() {
+        String result = "";
+        System.out.println("Input first String");
+        s1 = scanner.next();
+        System.out.println("Input second String");
+        s2 = scanner.next();
+
+        for (int i = 0; i < s1.length() && i < s2.length(); i++) {
+            if (s1.substring(0, i).equals(s2.substring(0, i))) {
+                result = s1.substring(0, i);
+            }
+        }
+        System.out.println("The common prefix is: " + result);
+        return result;
+    }
+
+}
---
+++ task-practice/src/main/java/longest/common/prefix/Main.java
@@ -0,0 +1,9 @@
+package longest.common.prefix;
+
+public class Main {
+
+    public static void main(String[] args) {
+        LongestCommonPrefix longestCommonPrefix = new LongestCommonPrefix();
+        longestCommonPrefix.getCommonPrefix();
+    }
+}
